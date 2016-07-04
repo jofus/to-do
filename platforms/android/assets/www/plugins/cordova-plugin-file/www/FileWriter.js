1a74dcc26e462d6f373fad593c10d7061478555b
Joey Scannell <joeyscannell@gmail.com>
2016-07-04 22:19:01 +0100

    android files

--- platforms/android/assets/www/plugins/cordova-plugin-file/www/FileWriter.js
+++ platforms/android/assets/www/plugins/cordova-plugin-file/www/FileWriter.js
@@ -1,4 +1,4 @@
-cordova.define("cordova-plugin-file.FileWriter", function(require, exports, module) { /*
+cordova.define("cordova-plugin-file.FileWriter", function(require, exports, module) { ﻿/*
  *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
